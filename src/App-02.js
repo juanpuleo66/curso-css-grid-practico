00f5f18497683c92155240449eb5ef76a3f86690
juanpuleo66 <juanpuleo@gmail.com>
2019-10-24 23:38:45 -0400

    102419-1135-pm

---
+++ src/App-02.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Grid-02.css';
+
+function App() {
+  return (
+      <main>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+      </main>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App-02';
+
+ReactDOM.render(<App />, document.getElementById('root'));
