6de7eec736fea0ac9018a57e86817c578849d194
Juan Puleo <juan.puleo@us.pwc.com>
2019-10-28 17:00:02 -0400

    102819-0459-PM

--- src/App-01.js
+++ src/App-01.js
@@ -4,13 +4,13 @@ import './Grid-01.css';
 function App() {
   return (
     <div>
-    <main>
-      <div className="cabecera">Cabecera</div>
-      <div className="izquierda">Izquierda</div>  
-      <div className="contenido">Contenido</div>
-      <div className="derecha">Derecha</div>  
-      <div className="piedepagina">Pie de pagina</div>
-    </main>
+      <main>
+        <div className="cabecera">Cabecera</div>
+        <div className="izquierda">Izquierda</div>  
+        <div className="contenido">Contenido</div>
+        <div className="derecha">Derecha</div>  
+        <div className="piedepagina">Pie de pagina</div>
+      </main>
     </div>
   );
 }
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './Grid.css';
+
+function App() {
+  return (
+    <main>
+      <div className="cabecera">Cabecera</div>
+      <div className="izquierda">Izquierda</div>  
+      <div className="contenido">Contenido</div>
+      <div className="derecha">Derecha</div>  
+      <div className="piedepagina">Pie de pagina</div>
+    </main>
+  );
+}
+
+export default App;
