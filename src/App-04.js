9844011858d9025828a9c28c6110a73bb8823b04
juanpuleo66 <juanpuleo@gmail.com>
2019-10-28 23:37:23 -0400

    102019-1137-PM

---
+++ src/App-04.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Grid-04.css';
+
+function App() {
+  return (
+    <main>
+      <header>Header</header>
+      <aside>sidebar</aside>  
+      <article>Contenido</article>
+      <footer>Footer</footer>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/App-06.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './Grid-06.css';
+
+function App() {
+  return (
+    <main>
+      <div>Elemento 01</div>
+      <div>Elemento 02</div>
+      <div>Elemento 03</div>
+      <div>Elemento 04</div>
+      <div>Elemento 05</div>
+      <div>Elemento 06</div>
+      <div>Elemento 07</div>
+      <div>Elemento 08</div>
+      <div>Elemento 09</div>
+    </main>
+  );
+}
+
+export default App;
